e2a1870462fe7fa1772450f2ba9d5cfac6b57522
adamfoda <afoda3@gmail.com>
2015-06-29 23:15:19 -0400

    use Illuminate\Http\Request;

--- laravel/app/Http/Controllers/Auth/AuthController.php
+++ laravel/app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\User;
 use Validator;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\AuthenticateUser as AuthenticateUser;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
