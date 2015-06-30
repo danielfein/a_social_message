824b316b5ca8cc5e483bd6f47a7d74183173a9c7
adamfoda <afoda3@gmail.com>
2015-06-29 23:58:46 -0400

    test print and Facebook

--- laravel/resources/views/welcome_twitter.blade.php
+++ laravel/resources/views/welcome_twitter.blade.php
@@ -46,7 +46,7 @@
             <div class="content">
                 <div class="title">Laravel 5</div>
                 <div class="quote">{{ Inspiring::quote() }}</div>
-              bitch we did it 
+              <? print_r($user); ?>
             </div>
         </div>
     </body>
