ba8fa15b2eab13d5d3394d5c4d4830a856f186cd
adamfoda <afoda3@gmail.com>
2015-06-30 00:01:50 -0400

    echo

--- laravel/resources/views/welcome_twitter.blade.php
+++ laravel/resources/views/welcome_twitter.blade.php
@@ -46,7 +46,7 @@
             <div class="content">
                 <div class="title">Laravel 5</div>
                 <div class="quote">{{ Inspiring::quote() }}</div>
-              <? print_r($user); ?>
+               {{ session('user') }}
             </div>
         </div>
     </body>
