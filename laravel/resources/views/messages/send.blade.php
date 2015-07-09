19412433af6a5a38bd025fc4f7602c0e9e14de4b
Daniel Fein <danieljfein@gmail.com>
2015-07-09 02:58:27 -0400

    no file upload

--- laravel/resources/views/messages/send.blade.php
+++ laravel/resources/views/messages/send.blade.php
@@ -9,7 +9,7 @@
 
           {!! Form::open(array('action' => 'MessagesController@receiveForm')) !!}
               {!! Form::text('message', "Enter your message here:") !!}
-              {!! Form::file('image') !!}
+            
 
               <br>
               {!! Form::submit('Click Me!') !!}
