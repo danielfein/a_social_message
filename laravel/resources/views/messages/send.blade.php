b13eda236ac89a8647337a254537eaf926ea830f
Daniel Fein <danieljfein@gmail.com>
2015-07-09 01:56:25 -0400

    y'all

--- laravel/resources/views/messages/send.blade.php
+++ laravel/resources/views/messages/send.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 
         @if(Auth::check())
-
+<div>
 
           {{ Form::open()}}
               {{Form::label('message', "Message:")}}
           {{ Form::close()}}
         @endif
-
+</div>
 @endsection
